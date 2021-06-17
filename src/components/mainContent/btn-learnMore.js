f1cd65a1a7b8954e70589b2620512bc15c7cd317
supervoid0 <shabee.neil@gmail.com>
2021-06-18 05:52:43 +0600

    Router Link added to buttons

--- src/components/mainContent/btn-learnMore.js
+++ src/components/mainContent/btn-learnMore.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-function BtnLearnMore(props) {
+function BtnLearnMore({path}) {
     return (
-        <button
-            style={{fontFamily:'Roboto'}}
-            className="border border-green-500 shadow-sm font-medium text-green-600 tracking-widest px-5 py-2 focus:outline-none hover:bg-green-500 hover:text-white"
-        >
-            LEARN MORE
-        </button>
+        <Link to={`${path}`}>
+            <button
+                style={{fontFamily:'Roboto'}}
+                className="border border-green-500 shadow-sm font-medium text-green-600 tracking-widest px-5 py-2 focus:outline-none hover:bg-green-500 hover:text-white"
+            >
+                LEARN MORE
+            </button>
+        </Link>
+        
     );
 }
 
